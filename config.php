7205df63a30344e2d7fa04b3b587879700117b64
saugat1 <38422801+saugat1@users.noreply.github.com>
2020-09-30 20:44:49 +0545

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 	session_start();
 	require 'Facebook/autoload.php';
 	$fb = new Facebook\Facebook([
-	  'app_id' => '633644487349953', // Replace {app-id} with your app id
+	  'app_id' => '327021888364464', // Replace {app-id} with your app id
 	  'app_secret' => '921a5ab627e492bcd56f80693a5aa02d', // Replace {app_secret} with your app secret
 	  'default_graph_version' => 'v8.0',
 	]);
